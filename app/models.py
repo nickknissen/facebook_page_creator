49cd35a516237f3339ef475cf104e1f3695ad8df
Nick Nissen <nickknissen@gmail.com>
2013-09-24 22:53:37 +0200

    update ui

--- app/models.py
+++ app/models.py
@@ -2,9 +2,24 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    nickname = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
-    password = db.Column(db.String(z))
+    password = db.Column(db.String())
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
 
     def __repr__(self):
-        return '<User %r>' % (self.nickname)
+        return '<User %r>' % (self.email)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
