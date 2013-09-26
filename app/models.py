228844c9950247b6f569dd12f737263eb247672d
Nick Nissen <nkn@grammatip.com>
2013-09-26 16:59:32 +0200

    rename username to email

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from app import db
+import datetime
+
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(120), index = True, unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String())
 
     def __init__(self, email, password):
@@ -10,7 +12,7 @@ class User(db.Model):
         self.password = password
 
     def __repr__(self):
-        return '<User %r>' % (self.email)
+        return '<User %r>' % self.email
 
     def is_authenticated(self):
         return True
@@ -23,3 +25,13 @@ class User(db.Model):
 
     def get_id(self):
         return unicode(self.id)
+
+
+class Content(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String())
+    timestamp = db.Column(db.DateTime)
+
+    def __init(self, body):
+        self.body = body
+        self.timestamp = datetime.datetime.utcnow()
