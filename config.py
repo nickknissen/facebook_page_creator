eb469f60d2e87f7563c5fc5a7947fc2e73d3ad0e
Nick Nissen <nickknissen@gmail.com>
2013-09-27 00:02:42 +0200

    update config for heroku

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
+if os.environ.get('HEROKU') is not None:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
