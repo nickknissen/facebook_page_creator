1b194fe8beea699fa09a30d5ad00b5a5946ffb57
Nick Nissen <nkn@grammatip.com>
2013-09-26 16:59:11 +0200

    add content model

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, g, request
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from werkzeug.security import generate_password_hash, \
     check_password_hash
@@ -6,47 +6,54 @@ from app import app, lm
 from app.forms import LoginForm
 from app.models import User, db
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html')
+    print(g.user)
+    if g.user is not None and g.user.is_authenticated():
 
-@app.route('/login', methods = ['GET', 'POST'])
+        return redirect(url_for('pages'))
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    f = request.form
     if current_user.is_authenticated():
         return redirect(url_for('pages'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('error validating')
-        return redirect('/index')
-    return render_template('login.html', 
-        title = 'Log ind',
-        form = form)
+        validate_user(f['email'], f['password'], f['remember_me'])
+    return render_template('login.html', title='Log ind', form=form)
 
-@login_required
+
 @app.route('/pages')
+@login_required
 def pages():
     return render_template('pages.html')
-
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
 
 def validate_user(email, password, remember_me=None):
     if email and password:
-        user = User.find_by(email=email).first()
+        user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                if remember_me:
-                    remember = None
-                else:
-                    remember = True
 
-                login_user(user, remember=remember)
+                login_user(user, remember=remember_me)
                 return user
+
 
 def create_user(email, password):
     user = User(email, generate_password_hash(password))
     db.session.add(user)
     db.session.commit()
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
